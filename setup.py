0f31dcf4f52b6a8d1dee193d3697aeea11a53449
theophanedroid <theodesile@gmail.com>
2022-01-26 18:11:41 +0100

    package stix2arango

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='stix2arango',
+    version='0.1.1',
+    author='theophanedroid',
+    description='stix2arango is a python package to convert stix2 to arangoDB',
+    packages=['stix2arango',],
+    license='MIT license',
+    long_description=open('README.md').read(),
+    install_requires=["stix2==3.0.1", "pyArango==1.3.5", "flask====2.0.2"]
+)
